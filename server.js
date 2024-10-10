d947f625437e22202b90a4d014164ac67265e1f3
Carlos Rene Perez G <d.londocreed@gmail.com>
2024-10-09 19:13:20 -0600

    v1

---
+++ server.js
@@ -0,0 +1,17 @@
+import express from 'express'
+import "dotenv/config.js"
+
+const server = express()
+const PORT = process.env.PORT || 8080
+const ready = () => console.log("The server is ready in " + PORT);
+server.listen(PORT, ready)
+
+server.get('/', (req, res) =>{
+    res.send('Hello, World!')
+})
+
+server.get('/saludo', (req, res) =>{
+    res.send('Hello, World this is new!')
+})
+
+console.log(process.env.PORT);
